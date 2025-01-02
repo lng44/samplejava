c0ed5e7e112de379def91428ee69e70ebf05f7ff
ext.lnig <ext.lnig@viessmann.com>
2025-01-02 16:56:15 +0700

    fix

--- init.ps1
+++ init.ps1
@@ -1,19 +1,15 @@
 Invoke-WebRequest -Uri https://download.java.net/openjdk/jdk17/ri/openjdk-17+35_windows-x64_bin.zip -OutFile C:\openjdk.zip;
-Expand-Archive -Path C:\openjdk.zip -DestinationPath C:\openjdk
+Expand-Archive -Path C:\openjdk.zip -DestinationPath C:\
 Remove-Item -Path C:\openjdk.zip
-Get-ChildItem -Path C:\openjdk | ForEach-Object {
-    if ($_.PSIsContainer) {
-        Write-Output "Directory: $($_.FullName)"
-    } else {
-        Write-Output "File: $($_.FullName)"
-    }
+Get-ChildItem -Path C:\ -Name *jdk-* | ForEach-Object {
+    Rename-Item -Path C:\$_ -NewName C:\openjdk
 }
 
 [System.Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\openjdk","Machine")
-Write-Output [System.Environment]::GetEnvironmentVariable("JAVA_HOME","Machine")
+Write-Output ${env:JAVA_HOME} 
 
-[System.Environment]::SetEnvironmentVariable("PATH", "C:\openjdk\bin;${env:PATH}","Machine")
-Write-Output [System.Environment]::GetEnvironmentVariable("PATH","Machine")
+[System.Environment]::SetEnvironmentVariable("PATH", "C:\openjdk\bin;"+[System.Environment]::GetEnvironmentVariable("PATH","Machine"),"Machine")
+Write-Output ${env:PATH} 
 
 Invoke-WebRequest -Uri https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip -OutFile C:\maven.zip
 Expand-Archive -Path C:\maven.zip -DestinationPath C:\
@@ -22,8 +18,8 @@ Get-ChildItem -Path C:\ -Name apache-maven-* | ForEach-Object {
     Rename-Item -Path C:\$_ -NewName C:\maven
 }
 
-[System.Environment]::SetEnvironmentVariable("PATH", "C:\maven\bin;${env:PATH}",[System.EnvironmentVariableTarget]::Machine)
-Write-Output [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("PATH", "C:\maven\bin;"+[System.Environment]::GetEnvironmentVariable("PATH","Machine"),"Machine")
+Write-Output ${env:PATH} 
 
 Invoke-WebRequest -Uri https://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.zip -OutFile C:\tomcat.zip
 Expand-Archive -Path C:\tomcat.zip -DestinationPath C:\
