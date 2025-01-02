9cddfb5ad5bfb2e3b743eb42b9efd198c263b72e
ext.lnig <ext.lnig@viessmann.com>
2025-01-02 22:28:21 +0700

    f18

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR c:\\app
 COPY . .
 RUN powershell -File "init.ps1"
 RUN mvn clean package
-RUN copy c:\\app\\target\\*.war C:\\tomcat\\webapps\\ROOT.war
+RUN copy c:\\app\\target\\*.war C:\\tomcat\\webapps\\ROOT.zip
 RUN powershell -File "debug.ps1"
 EXPOSE 8080
 CMD ["catalina.bat","run"]
--- debug.ps1
+++ debug.ps1
@@ -2,5 +2,5 @@ Get-ChildItem -Path C:\openjdk
 Get-ChildItem -Path C:\maven
 Get-ChildItem -Path C:\tomcat
 Get-ChildItem -Path C:\tomcat\webapps
-Expand-Archive -Path C:\tomcat\webapps\ROOT.war -DestinationPath C:\tomcat\webapps\ROOT
+Expand-Archive -Path C:\tomcat\webapps\ROOT.zip -DestinationPath C:\tomcat\webapps\ROOT
 Get-ChildItem -Path C:\tomcat\webapps\ROOT
