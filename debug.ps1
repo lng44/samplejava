8eefd296774b77c1124f482cc10a9c87bc93f14c
ext.lnig <ext.lnig@viessmann.com>
2025-01-02 22:49:06 +0700

    f21

--- debug.ps1
+++ debug.ps1
@@ -2,6 +2,6 @@ Get-ChildItem -Path C:\openjdk
 Get-ChildItem -Path C:\maven
 Get-ChildItem -Path C:\tomcat
 Get-ChildItem -Path C:\tomcat\webapps
-& jar xf C:\tomcat\webapps\ROOT.war -C C:\tomcat\webapps\ROOT
+# & jar xf C:\tomcat\webapps\ROOT.war -C C:\tomcat\webapps\ROOT
 # Expand-Archive -Path  -DestinationPath C:\tomcat\webapps\ROOT
 Get-ChildItem -Path C:\tomcat\webapps\ROOT
