3b6797f8707195466edd8e037710db02906bf6e0
ext.lnig <ext.lnig@viessmann.com>
2025-01-02 16:31:04 +0700

    fix

--- init.ps1
+++ init.ps1
@@ -2,11 +2,11 @@ Invoke-WebRequest -Uri https://download.java.net/openjdk/jdk17/ri/openjdk-17+35_
 Expand-Archive -Path C:\openjdk.zip -DestinationPath C:\openjdk
 Remove-Item -Path C:\openjdk.zip
 
-[System.Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\openjdk",[System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\openjdk",[System.EnvironmentVariableTarget]::Machine)
 Write-Output [System.Environment]::GetEnvironmentVariable("JAVA_HOME", [System.EnvironmentVariableTarget]::Machine)
 
 Write-Output ${env:PATH}
-[System.Environment]::SetEnvironmentVariable("PATH", "C:\openjdk\bin;${env:PATH}",[System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("PATH", "C:\openjdk\bin;${env:PATH}",[System.EnvironmentVariableTarget]::Machine)
 Write-Output [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
 
 Invoke-WebRequest -Uri https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip -OutFile C:\maven.zip
@@ -17,7 +17,7 @@ Get-ChildItem -Path C:\ -Name apache-maven-* | ForEach-Object {
 }
 
 Write-Output [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("PATH", "C:\maven\bin;${env:PATH}",[System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("PATH", "C:\maven\bin;${env:PATH}",[System.EnvironmentVariableTarget]::Machine)
 Write-Output [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
 
 Invoke-WebRequest -Uri https://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.zip -OutFile C:\tomcat.zip
