eb006b340c8a7741d42db42b5d938cd3068a9124
ext.lnig <ext.lnig@viessmann.com>
2025-01-02 16:32:01 +0700

    fix

--- init.ps1
+++ init.ps1
@@ -3,11 +3,11 @@ Expand-Archive -Path C:\openjdk.zip -DestinationPath C:\openjdk
 Remove-Item -Path C:\openjdk.zip
 
 [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\openjdk",[System.EnvironmentVariableTarget]::Machine)
-Write-Output [System.Environment]::GetEnvironmentVariable("JAVA_HOME", [System.EnvironmentVariableTarget]::Machine)
+Write-Output [Environment]::GetEnvironmentVariable("JAVA_HOME", [System.EnvironmentVariableTarget]::Machine)
 
 Write-Output ${env:PATH}
 [Environment]::SetEnvironmentVariable("PATH", "C:\openjdk\bin;${env:PATH}",[System.EnvironmentVariableTarget]::Machine)
-Write-Output [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
+Write-Output [Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
 
 Invoke-WebRequest -Uri https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.zip -OutFile C:\maven.zip
 Expand-Archive -Path C:\maven.zip -DestinationPath C:\
@@ -16,9 +16,9 @@ Get-ChildItem -Path C:\ -Name apache-maven-* | ForEach-Object {
     Rename-Item -Path C:\$_ -NewName C:\maven
 }
 
-Write-Output [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
+Write-Output [Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
 [Environment]::SetEnvironmentVariable("PATH", "C:\maven\bin;${env:PATH}",[System.EnvironmentVariableTarget]::Machine)
-Write-Output [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
+Write-Output [Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
 
 Invoke-WebRequest -Uri https://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.zip -OutFile C:\tomcat.zip
 Expand-Archive -Path C:\tomcat.zip -DestinationPath C:\
