6a6df064736bca6d2bd95979edc4fc0735504c2f
ext.lnig <ext.lnig@viessmann.com>
2025-01-03 13:15:21 +0700

    debug5

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM mcr.microsoft.com/windows/servercore:ltsc2019
-RUN powershell -Command "Get-ChildItem -Path ."
-RUN powershell -Command "Get-ChildItem -Path C:\Users"
-RUN powershell -Command "Get-LocalUser"
-RUN powershell -Command "Get-LocalGroup | ft Name, SID"
-RUN powershell -Command "Set-ExecutionPolicy RemoteSigned"
+# RUN powershell -Command "Get-ChildItem -Path ."
+# RUN powershell -Command "Get-ChildItem -Path C:\Users"
+# RUN powershell -Command "Get-LocalUser"
+# RUN powershell -Command "Get-LocalGroup | ft Name, SID"
+# RUN powershell -Command "Set-ExecutionPolicy RemoteSigned"
 USER ContainerUser
 RUN MKDIR c:\\app
 WORKDIR c:/app
@@ -13,12 +13,12 @@ USER ContainerAdministrator
 RUN tzutil /s "Central Standard Time"
 RUN powershell -File "init.ps1"
 RUN mvn clean package
+USER ContainerUser
 RUN copy c:\\app\\target\\*.war c:\\app\\tomcat\\webapps
 # RUN MKDIR c:\\tomcat\\webapps\\ROOT
 # WORKDIR c:/tomcat/webapps/ROOT
 # RUN jar -xvf ..\\ROOT.war
 # WORKDIR c:/app
-RUN powershell -File "debug.ps1"
+# RUN powershell -File "debug.ps1"
 EXPOSE 8080
-USER ContainerUser
 CMD ["catalina.bat","run"]
--- init.ps1
+++ init.ps1
@@ -1,22 +1,22 @@
 
-try {
-    $aclPath = $path + 'C:\app\tomcat'
-    Get-Acl -Path $aclPath
-    Write-Host "Changing permissions for: $aclPath"
-    $objUser = New-Object System.Security.Principal.NTAccount(".", "Users")
-    $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
-    Write-Host "SID value is: " $strSID
-    $permissions = 'FullControl'
-    $inheritance = 'ContainerInherit, ObjectInherit'
-    $acl = Get-Acl -Path $aclPath
-    $rule = new-object System.Security.AccessControl.FileSystemAccessRule($strSID, $permissions, $inheritance, 'InheritOnly', 'Allow')
-    $acl.AddAccessRule($rule)
-    Set-Acl -AclObject $acl -Path $aclPath
-    Write-Host "Access control change successful" -fore green
-}
-catch {
-    Write-Host "Error setting access control: $_" -fore red
-}
+# try {
+#     $aclPath = $path + 'C:\app\tomcat'
+#     Get-Acl -Path $aclPath
+#     Write-Host "Changing permissions for: $aclPath"
+#     $objUser = New-Object System.Security.Principal.NTAccount(".", "Users")
+#     $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+#     Write-Host "SID value is: " $strSID
+#     $permissions = 'FullControl'
+#     $inheritance = 'ContainerInherit, ObjectInherit'
+#     $acl = Get-Acl -Path $aclPath
+#     $rule = new-object System.Security.AccessControl.FileSystemAccessRule($strSID, $permissions, $inheritance, 'InheritOnly', 'Allow')
+#     $acl.AddAccessRule($rule)
+#     Set-Acl -AclObject $acl -Path $aclPath
+#     Write-Host "Access control change successful" -fore green
+# }
+# catch {
+#     Write-Host "Error setting access control: $_" -fore red
+# }
 [System.Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\app\openjdk","Machine")
 [System.Environment]::SetEnvironmentVariable("PATH", "C:\app\openjdk\bin;"+[System.Environment]::GetEnvironmentVariable("PATH","Machine"),"Machine")
 [System.Environment]::SetEnvironmentVariable("PATH", "C:\app\maven\bin;"+[System.Environment]::GetEnvironmentVariable("PATH","Machine"),"Machine")
